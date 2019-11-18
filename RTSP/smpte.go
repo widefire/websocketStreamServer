1d2d947fb164300d07cf4fd939e50f95c2913c90
yehuolaile <yehuolaile@outlook.com>
2019-11-18 21:00:45 +0800

    1

--- RTSP/smpte.go
+++ RTSP/smpte.go
@@ -1,4 +1,10 @@
 package RTSP
 
 //https://blog.csdn.net/andrew57/article/details/6752182
+/*
+smpte=10:12:33:20-
+smpte=10:07:33-
+smpte=10:07:00-10:07:33:05.01
+smpte-25=10:07:00-10:07:33:05.01
+*/
 const SMPTE_30_drop_frame_rate = 29.97
