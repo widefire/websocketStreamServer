c00925a29602fd23ddc2d5f3bae99cc1fd05bf54
yehuolaile <yehuolaile@outlook.com>
2019-11-19 22:07:53 +0800

    end test smpte

--- RTSP/smpte.go
+++ RTSP/smpte.go
@@ -23,10 +23,12 @@ const smpte_prefix = "smpte"
 
 var regType *regexp.Regexp
 var regInt *regexp.Regexp
+var regTwoDigit *regexp.Regexp
 
 func init() {
 	regType = regexp.MustCompile("smpte-[0-9]*=")
 	regInt = regexp.MustCompile("[1-9][0-9]*")
+	regTwoDigit = regexp.MustCompile("[0-9][0-9]")
 }
 
 func IsSMPTE(line string) bool {
@@ -66,7 +68,7 @@ func ParseSMPTE(line string) (err error, drop bool, frameRate interface{}, from,
 	eqIndex := strings.Index(line, "=")
 	hyphenIndex := strings.Index(line, "-")
 
-	prefix := "smpte"
+	prefix := "smpte="
 	if hyphenIndex < eqIndex {
 		drop = false
 		prefix = regType.FindString(line)
@@ -99,14 +101,20 @@ func ParseSMPTE(line string) (err error, drop bool, frameRate interface{}, from,
 
 	//from
 	if len(fromToArr[0]) > 0 {
-
+		err, from = parseSampteRange(fromToArr[0])
+		if err != nil {
+			return
+		}
 	} else {
 		err = errors.New("empty sampte from range")
 		return
 	}
 	//to
 	if len(fromToArr[1]) > 0 {
-
+		err, to = parseSampteRange(fromToArr[1])
+		if err != nil {
+			return
+		}
 	} else {
 		to = nil
 	}
@@ -121,16 +129,12 @@ func parseSampteRange(strRange string) (err error, ts *Smpte_timestamp) {
 
 	c := len(subValues)
 
-	if c <= 3 {
-		err = errors.New("smpte range at least has h,m,s")
-		return
-	}
-	if c > 4 {
-		err = errors.New("smpte range too many \":\"")
+	if c != 3 && c != 4 {
+		err = errors.New("too many or less : for range")
 		return
 	}
 
-	if len(subValues[0]) == 0 || len(subValues[1]) == 0 || len(subValues[2]) == 0 {
+	if len(subValues[0]) != 2 || len(subValues[1]) != 2 || len(subValues[2]) != 2 {
 		err = errors.New("smpte range h,m,s can not empty")
 		return
 	}
@@ -151,19 +155,27 @@ func parseSampteRange(strRange string) (err error, ts *Smpte_timestamp) {
 	}
 
 	if c == 4 && len(subValues[3]) > 0 {
-		frame_subFrame := regInt.FindAllString(subValues[3], -1)
+		frame_subFrame := regTwoDigit.FindAllString(subValues[3], -1)
 		count_frame_subFrame := len(frame_subFrame)
 		if count_frame_subFrame == 0 || count_frame_subFrame > 2 {
 			err = errors.New("smpte range invalid frame subframe")
 			return
 		}
 		if count_frame_subFrame > 0 {
+			if len(frame_subFrame[0]) != 2 {
+				err = errors.New("need two digit")
+				return
+			}
 			ts.Frames, err = strconv.Atoi(frame_subFrame[0])
 			if err != nil {
 				return
 			}
 		}
 		if count_frame_subFrame > 1 {
+			if len(frame_subFrame[1]) != 2 {
+				err = errors.New("need two digit")
+				return
+			}
 			ts.Subframes, err = strconv.Atoi(frame_subFrame[1])
 			if err != nil {
 				return
--- RTSP/smpte_test.go
+++ RTSP/smpte_test.go
@@ -12,13 +12,78 @@ Examples:
      smpte-25=10:07:00-10:07:33:05.01
 */
 
-func TestCheckSMPTE(t *testing.T) {
+func TestCheckSMPTEOnlyFrom(t *testing.T) {
 	t1 := "smpte=10:12:33:20-"
 	if !IsSMPTE(t1) {
 		t.Error("check is smpte failed")
 	}
 
-	ParseSMPTE(t1)
+	err, drop, frameRate, from, to := ParseSMPTE(t1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !drop {
+		t.Error("should drop")
+	}
+
+	ffr, ok := frameRate.(float64)
+	if !ok {
+		t.Error("not float32")
+	}
+	if ffr != SMPTE_30_drop_frame_rate {
+		t.Error("bad frame rate")
+	}
+
+	if from == nil {
+		t.Error("from should not nil")
+	}
+
+	if from.Hours != 10 || from.Minutes != 12 || from.Seconds != 33 || from.Frames != 20 || from.Subframes != 0 {
+		t.Error("parse time failed ")
+	}
+
+	if to != nil {
+		t.Error("t0 should nil")
+	}
+
+	return
+}
+
+func TestCheckSMPTEFromTo(t *testing.T) {
+	t1 := "smpte-25=10:07:00-10:07:33:05.01"
+	if !IsSMPTE(t1) {
+		t.Error("check is smpte failed")
+	}
+
+	err, drop, frameRate, from, to := ParseSMPTE(t1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if drop {
+		t.Error("should not drop")
+	}
+
+	ffr, ok := frameRate.(int)
+	if !ok {
+		t.Error("not float32")
+	}
+	if ffr != 25 {
+		t.Error("bad frame rate")
+	}
+
+	if from == nil {
+		t.Error("from should not nil")
+	}
+
+	if from.Hours != 10 || from.Minutes != 7 || from.Seconds != 0 || from.Frames != 0 || from.Subframes != 0 {
+		t.Error("parse time failed ")
+	}
+
+	if to == nil {
+		t.Error("t0 should not nil")
+	}
 
 	return
 }
