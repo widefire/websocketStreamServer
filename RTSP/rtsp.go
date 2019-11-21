2c9d4959ee50cf8fb1f4c67be026e1433855447f
zcy <695215606@qq.com>
2019-11-21 18:31:48 +0800

    添加 RTSP方法

---
+++ RTSP/rtsp.go
@@ -0,0 +1,15 @@
+package RTSP
+
+const (
+	RTSP_Method_DESCRIBE      = "DESCRIBE"
+	RTSP_Method_ANNOUNCE      = "ANNOUNCE"
+	RTSP_Method_GET_PARAMETER = "GET_PARAMETER"
+	RTSP_Method_OPTIONS       = "OPTIONS"
+	RTSP_Method_PAUSE         = "PAUSE"
+	RTSP_Method_PLAY          = "PLAY"
+	RTSP_Method_RECORD        = "RECORD"
+	RTSP_Method_REDIRECT      = "REDIRECT"
+	RTSP_Method_SETUP         = "SETUP"
+	RTSP_Method_SET_PARAMETER = "PARAMETER"
+	RTSP_Method_SET_TEARDOWN  = "TEARDOWN"
+)
