c78682edae61dd2b2b6a383d1918e6329d7622a8
zcy <695215606@qq.com>
2019-11-25 18:29:51 +0800

    begin sdp

--- sdp/sdp.go
+++ sdp/sdp.go
@@ -1,1 +1,19 @@
 package sdp
+
+//https://blog.csdn.net/china_jeffery/article/details/79991986
+//rfc 4566
+type SessionDescription struct {
+	ProtocolVersion int
+	Origin          Origin
+	SessionName     string
+	
+}
+
+type Origin struct {
+	Username       string
+	SessionId      uint64
+	SessionVersion uint64
+	NetType        string
+	AddrType       string
+	UnicastAddress string
+}
